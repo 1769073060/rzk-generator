ff8f123f594cfec2680483423f0c3ea7a72c4e95
lqz <837010582@qq.com>
2022-05-20 20:39:56 +0800

    feat:添加代码生成项目

---
+++ rzk-generator/src/main/java/com/rzk/GeneratorApplication.java
@@ -0,0 +1,16 @@
+package com.rzk;
+
+/**
+ * @PackageName : com.rzk
+ * @FileName : GeneratorApplication
+ * @Description : 启动类
+ * @Author : rzk
+ * @CreateTime : 2022/5/20 20:32
+ * @Version : 1.0.0
+ */
+@SpringBootApplication
+public class GeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class,args);
+    }
+}
